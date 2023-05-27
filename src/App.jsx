ef12503f947c7b1788c09816659c6753a560c6ec
ppratap2000 <pranavstudy2022@gmail.com>
2023-05-27 23:52:54 +0530

    Home page remaining changes done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ProductList from "./pages/ProductList";
+
+const App = () => {
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  border: 1px solid white;
+  padding: 10px;
+  background-color: transparent;
+  color: whitesmoke;
+  cursor: pointer;
+  font-weight: 600;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+`;
+const CategoryItem = ({ item }) => {
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
+  );
+};
+
+export default CategoryItem;
